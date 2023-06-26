80756e7d5aefa236ccebc1f65d0a8223a6bceff9
Voronov <dmitriy.woronow@gmail.com>
2023-06-26 11:20:13 +0300

    log lib

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"os"
+	"url-shortener/internal/config"
+	"url-shortener/internal/lib/logger/sl"
+
+	"url-shortener/internal/storage/sqlite"
+
+	"golang.org/x/exp/slog"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env))
+
+	log.Info("initializing server", slog.String("address", cfg.Address))
+	log.Debug("logger debug enabled")
+
+	_, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to initialize storage", sl.Err(err))
+	}
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev: //envProd
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
